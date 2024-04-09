733bedc09d798056e4f7b83f50c8884fa2346e1d
larchdeciduous <scrolls-larches.0v@icloud.com>
2024-04-09 04:26:34 -0400

    update react

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,11 +2,12 @@
 # import requests
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    'backend_url', default="http://localhost:3030/")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
@@ -16,17 +17,18 @@ def get_request(endpoint, **kwargs):
     if(kwargs):
         for key,value in kwargs.items():
             params=params+key+"="+value+"&"
-
-    request_url = backend_url+endpoint+"?"+params
+        request_url = backend_url+endpoint+"?"+params
+    else:
+        request_url = backend_url+endpoint
 
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
-        # If any error occurs
-        print("Network exception occurred")  
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred") 
 # Add code for get requests to back end
 
 def analyze_review_sentiments(text):
